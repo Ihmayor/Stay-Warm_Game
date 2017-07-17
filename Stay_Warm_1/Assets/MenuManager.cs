3a153449c4d2c91055cd82ab359f95c797d737be
Irene Mayor <ihmayor@ucalgary.ca>
2017-07-17 17:41:45 -0600

    Added Sound to menu popup. Add shrink open function to see how that would look.

--- Stay_Warm_1/Assets/MenuManager.cs
+++ Stay_Warm_1/Assets/MenuManager.cs
@@ -12,12 +12,14 @@ public class MenuManager : MonoBehaviour {
     private GameObject MainMenuSystem;
     private GameObject InstructionBox;
 
+    private float MenuRatio;
+    private AudioSource[] AudioSources;
     // Use this for initialization
     void Start () {
         MenuManager.Instance = this;
         MainMenuSystem = GameObject.Find("MenuSystem");
         InstructionBox = MainMenuSystem.transform.Find("GoofyPlaceHolderUI").transform.Find("InstructionsPanel").gameObject;
-
+        AudioSources = InstructionBox.GetComponents<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,12 +31,35 @@ public class MenuManager : MonoBehaviour {
     {
         InstructionBox = MainMenuSystem.transform.Find("GoofyPlaceHolderUI").transform.Find("InstructionsPanel").gameObject;
         InstructionBox.transform.Find("Text").gameObject.GetComponent<Text>().text = message;
-        InstructionBox.SetActive(true);
-        Invoke("CloseInstructionBox", 5f);
+        InstructionBox.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+        MenuRatio = 1 / 0.1f;
+        StartCoroutine("BiggerMenu");
     }
 
-    private void CloseInstructionBox()
+
+    IEnumerator BiggerMenu()
+   {
+        InstructionBox.SetActive(true);
+        AudioSources[0].Play();
+        for (float i = 0; i <= MenuRatio; i++)
+        {
+            yield return new WaitForSeconds(0.01f);
+            InstructionBox.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+            yield return null;
+        }
+        StartCoroutine("CloseInstructionBox");
+    }
+
+    private IEnumerator CloseInstructionBox()
     {
+        yield return new WaitForSeconds(3.5f);
+        AudioSources[1].Play();
+        for (float i = MenuRatio/2; i >= 0; i--)
+        {
+            yield return new WaitForSeconds(0.01f);
+            InstructionBox.transform.localScale -= new Vector3(0.2f, 0.2f, 0.2f);
+            yield return null;
+        }
         InstructionBox.SetActive(false);
     }
 }
