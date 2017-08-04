89d4cf3adf00501ec264a02702ae21d3455962af
Irene Mayor <ihmayor@ucalgary.ca>
2017-08-03 22:30:48 -0600

    Cleaning Circular Map Elevation Code

---
+++ Stay_Warm_1/Assets/AreaEffects.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffects : MonoBehaviour
+{
+
+
+    public float test = 0f;
+    public string[] ThoughtsInArea;
+    private int ThoughtCount = -1;
+
+    private float SinceLastThought;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (ThoughtsInArea.Length > 0)
+        {
+            ThoughtCount = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("AAAAA");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("WHAAAAAA");  
+        float currentTime = Time.fixedTime;
+        if (collision.gameObject.tag.Contains("Player"))
+        {
+            GameObject player = collision.gameObject;
+
+            //Set time since last thought
+            if (SinceLastThought == 0)
+            {
+                SinceLastThought = currentTime;
+                return;
+            }
+
+            float timeDifference = currentTime - SinceLastThought;
+
+            //Check if we have any thoughts
+            if (ThoughtCount >= 0 && timeDifference > 3)
+            {
+                MenuManager.Instance.SetThought(player.GetComponent<CharacterStatus>()., ThoughtsInArea[ThoughtCount]);
+
+                if (ThoughtCount + 1 < ThoughtsInArea.Length)
+                    ThoughtCount++;
+
+                SinceLastThought = currentTime;
+            }
+        }
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+    }
+}
