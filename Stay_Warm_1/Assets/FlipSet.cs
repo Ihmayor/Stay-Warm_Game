170e869747237cfe060a925d6d978f5ce7a1e99d
Irene Mayor <ihmayor@ucalgary.ca>
2017-09-09 07:00:27 -0600

    Fixed Minor Flipping Bug.

---
+++ Stay_Warm_1/Assets/FlipSet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Is this being called");    
+    }
+
+   
+}
