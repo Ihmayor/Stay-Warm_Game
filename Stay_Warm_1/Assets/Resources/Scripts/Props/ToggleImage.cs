db3af799be9c8a813c460fa4976eec9eea90f18b
Irene Mayor <ihmayor@ucalgary.ca>
2017-09-11 12:14:07 -0600

    Implemented Base Menu System Transitions

---
+++ Stay_Warm_1/Assets/Resources/Scripts/Props/ToggleImage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleImage : MonoBehaviour {
+    public Sprite Image1;
+    public Sprite Image2;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Toggle()
+    {
+        if (gameObject.GetComponent<Image>().sprite == Image1)
+        {
+            gameObject.GetComponent<Image>().sprite = Image2;
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().sprite = Image1;
+        }
+        this.GetComponent<Animator>().ResetTrigger("Highlighted");
+        gameObject.SetActive(false);
+        gameObject.SetActive(true);
+        this.GetComponent<Animator>().SetTrigger("Normal");
+
+    }
+
+
+}
