26714c00ae4dbb5c6a03c7e1b68d9652b6f9ab86
Ihmayor <ihmayor@ucalgary.ca>
2018-08-21 21:38:39 -0600

    Disabled the button

---
+++ Stay_Warm_1/Assets/LoadLevelButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadLevelButtonScript : MonoBehaviour {
+
+
+    public bool Disabled = false;
+	// Use this for initialization
+	void Start () {
+		//Respond to load save file or have code in here. 
+        if (Disabled)
+        {
+            Color panelColor;
+            ColorUtility.TryParseHtmlString("#323232", out panelColor);
+            Color textColor;
+            ColorUtility.TryParseHtmlString("#ABADAE", out textColor);
+
+            gameObject.GetComponent<Animator>().enabled = false;
+            gameObject.GetComponent<Image>().color = panelColor;
+            gameObject.transform.GetComponentInChildren<Text>().color = textColor;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
