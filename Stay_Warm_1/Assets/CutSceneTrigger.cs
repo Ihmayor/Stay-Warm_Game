cc47c9cb0c12dae29d03657d4084cf4e14a61962
Ihmayor <ihmayor@ucalgary.ca>
2018-08-26 18:49:48 -0600

    Added Cinemachine Gizmos + Scene Tested Cutscene

---
+++ Stay_Warm_1/Assets/CutSceneTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutSceneTrigger : MonoBehaviour {
+
+    public PlayableDirector timeline;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.CompareTag("Player"))
+        {
+            timeline.Stop(); // Make sure the timeline is stopped before starting it
+            timeline.Play();
+        }
+    }
+}
