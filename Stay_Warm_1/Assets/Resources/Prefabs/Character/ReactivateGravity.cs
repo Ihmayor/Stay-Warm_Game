7da5ce9af47722bb22695a42d54acc33dc7c05be
Ihmayor <ihmayor@ucalgary.ca>
2018-08-26 23:40:47 -0600

    Added Intro Scene

---
+++ Stay_Warm_1/Assets/Resources/Prefabs/Character/ReactivateGravity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ReactivateGravity : MonoBehaviour {
+
+    PlayableDirector timeline;
+    public GameObject Character;
+	// Use this for initialization
+	void Start () {
+        Character.GetComponent<CharacterMovement>().enabled = false;
+        timeline = GetComponent<PlayableDirector>();
+        timeline.stopped += ReactivateGravity_stopped;
+	}
+
+    private void ReactivateGravity_stopped(PlayableDirector obj)
+    {
+        Character.GetComponent<Rigidbody2D>().gravityScale = 1;
+        Character.GetComponent<CharacterMovement>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
